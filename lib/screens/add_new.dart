e4b65afcc4ced0920eec0f89b249810c9f58c7fe
Sakuntha sankalpa <sakunthasankalpa67@gmail.com>
2025-04-10 12:34:04 +0530

    add new page text form fields

--- lib/screens/add_new.dart
+++ lib/screens/add_new.dart
@@ -1,4 +1,6 @@
 import 'package:expenze_application/constents/colors.dart';
+import 'package:expenze_application/models/expence_model.dart';
+import 'package:expenze_application/models/income_model.dart';
 import 'package:flutter/material.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -10,8 +12,23 @@ class AddNewScreen extends StatefulWidget {
 
 class _AddNewScreenState extends State<AddNewScreen> {
   //sate to track the expence or income
-
   int _selectedMethord = 0;
+
+  Expencecatagories _expencecatagories = Expencecatagories.food;
+  IncomeCatagory _incomeCatagory = IncomeCatagory.pasive;
+
+  //textfeild controller
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _disctriptionController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _disctriptionController.dispose();
+    _amountController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +41,7 @@ class _AddNewScreenState extends State<AddNewScreen> {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 22),
                 child: Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
                   height: MediaQuery.of(context).size.height * 0.06,
                   decoration: BoxDecoration(
                     color: kWhite,
@@ -88,6 +106,146 @@ class _AddNewScreenState extends State<AddNewScreen> {
                         ),
                       ),
                     ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "How Much?",
+                        style: TextStyle(
+                          color: kLightGrey,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.w700,
+                          color: kWhite,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "0",
+                          border: InputBorder.none,
+                          hintStyle: TextStyle(color: kLightGrey, fontSize: 60),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.25,
+                ),
+                height: MediaQuery.of(context).size.height * 0.6,
+                decoration: BoxDecoration(
+                  color: kWhite,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 10,
+                  ),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        //chatagory selecter dropdown
+                        DropdownButtonFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                          items:
+                              _selectedMethord == 1
+                                  ? Expencecatagories.values.map((category) {
+                                    return DropdownMenuItem(
+                                      value: category,
+                                      child: Text(category.name),
+                                    );
+                                  }).toList()
+                                  : IncomeCatagory.values.map((category) {
+                                    return DropdownMenuItem(
+                                      value: category,
+                                      child: Text(category.name),
+                                    );
+                                  }).toList(),
+                          value:
+                              _selectedMethord == 0
+                                  ? _incomeCatagory
+                                  : _expencecatagories,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedMethord == 0
+                                  ? _expencecatagories =
+                                      value as Expencecatagories
+                                  : _incomeCatagory = value as IncomeCatagory;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 25),
+                        TextFormField(
+                          controller: _titleController,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 17,
+                            ),
+                            hintText: "Title",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 25),
+
+                        TextFormField(
+                          controller: _disctriptionController,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 17,
+                            ),
+                            hintText: "Discription",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 25),
+
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: _amountController,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 17,
+                            ),
+                            hintText: "Amount",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
